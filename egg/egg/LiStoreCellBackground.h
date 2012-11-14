81af6289a47dbc2d060ec91c882b93047a892432
Bob Waycott <bobwaycott@gmail.com>
2012-11-14 12:15:25 -0500

    Add new LiStoreCellBackground class that delivers a rounded, colored rect for LiStoreCell's selectedBackgroundView property

---
+++ egg/egg/LiStoreCellBackground.h
@@ -0,0 +1,13 @@
+//
+//  LiStoreCellBackground.h
+//  egg
+//
+//  Created by Bob Waycott on 11/14/12.
+//  Copyright (c) 2012 Applicasa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LiStoreCellBackground : UIView
+
+@end
---
+++ egg/egg/LiStoreCellBackground.m
@@ -0,0 +1,26 @@
+//
+//  LiStoreCellBackground.m
+//  egg
+//
+//  Created by Bob Waycott on 11/14/12.
+//  Copyright (c) 2012 Applicasa. All rights reserved.
+//
+
+#import "LiStoreCellBackground.h"
+
+@implementation LiStoreCellBackground
+
+- (void)drawRect:(CGRect)rect
+{
+    
+    // draw a rounded rect bezier path filled with blue
+    CGContextRef aRef = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(aRef);
+    UIBezierPath *bezierPath = [UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:5.0f];
+    UIColor *fillColor = [UIColor colorWithRed:0.24 green:0.52 blue:0.78 alpha:1.0];
+    [fillColor setFill];
+    [bezierPath fill];
+    CGContextRestoreGState(aRef);
+}
+
+@end
