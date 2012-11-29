20898f622ef4396cd11406ee7e3d8689708928f3
benny <benny@applicasa.com>
2012-11-29 18:48:44 +0200

    Promotion show changed according to new SDK & Framework

---
+++ ApplicasaSDK/Promo/LiPromo.h
@@ -0,0 +1,13 @@
+//
+//  LiPromo.h
+//  egg
+//
+//  Created by Benny Davidovitz on 11/29/12.
+//  Copyright (c) 2012 Applicasa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LiPromo : NSObject
+
+@end
---
+++ ApplicasaSDK/Promo/LiPromo.m
@@ -0,0 +1,13 @@
+//
+//  LiPromo.m
+//  egg
+//
+//  Created by Benny Davidovitz on 11/29/12.
+//  Copyright (c) 2012 Applicasa. All rights reserved.
+//
+
+#import "LiPromo.h"
+
+@implementation LiPromo
+
+@end
